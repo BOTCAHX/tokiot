1c16224e7fa815fda35d7ded93a6f2489865a814
Tio Permana <94074076+BOTCAHX@users.noreply.github.com>
2021-12-29 18:48:38 +0700

    Update tokio.js

--- plugins/tokio.js
+++ plugins/tokio.js
@@ -1,5 +1,5 @@
 let handler = async m => m.reply(`
-☕ *Tokio :* a whatsapp bot\n\n🔗 *URL :* https://github.com/itsajaygaur/tokio-wabot
+☕ *Tokio :* a whatsapp bot\n\n🔗 *URL :* Hamdeh nyari sc sendiri lah Asu
 `.trim()) // repository
 handler.help = ['tokio']
 handler.tags = ['info']
